b02b7fa7938a61716864d034f60419c97536555d
ruizaj <ruizaj@hawaii.edu>
2020-04-20 21:56:54 -1000

    issue-16: still need to implement payment options

--- app/imports/ui/pages/Signup.jsx
+++ app/imports/ui/pages/Signup.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link, Redirect } from 'react-router-dom';
-import { Container, Form, Grid, Header, Message, Segment } from 'semantic-ui-react';
+import { Container, Form, Grid, Header, Message, Segment, Divider } from 'semantic-ui-react';
 import { Accounts } from 'meteor/accounts-base';
 
 /**
@@ -38,18 +38,27 @@ class Signup extends React.Component {
     if (this.state.redirectToReferer) {
       return <Redirect to={from}/>;
     }
+
+
+    const signContainer = {
+      paddingBottom: '5rem',
+    };
+
     return (
-        <Container>
+        <Container style={signContainer}>
           <Grid textAlign="center" verticalAlign="middle" centered columns={2}>
             <Grid.Column>
-              <Header as="h2" textAlign="center">
-                Account Information
-              </Header>
+              <Divider horizontal>
+               <Header inverted as="h2" textAlign="center">
+                 Account Information
+               </Header>
+              </Divider>
               <Form onSubmit={this.submit}>
-                <Segment stacked>
+                <Segment className='signup-form' stacked>
                   <Form.Group widths='equal'>
                     <Form.Input
                         fluid label='First Name'
+                        name='firstName'
                         placeholder='First Name'
                         onChange={this.handleChange}
                     />
@@ -60,71 +69,82 @@ class Signup extends React.Component {
                     />
                   </Form.Group>
                   <Form.Input
-                      label="Email"
-                      icon="user"
-                      iconPosition="left"
-                      name="email"
-                      type="email"
-                      placeholder="E-mail address"
+                      label='Email'
+                      icon='user'
+                      iconPosition='left'
+                      name='email'
+                      type='email'
+                      placeholder='E-mail address'
                       onChange={this.handleChange}
                   />
                   <Form.Input
-                      label="Password"
-                      icon="lock"
-                      iconPosition="left"
-                      name="password"
-                      placeholder="Password"
-                      type="password"
+                      label='Password'
+                      icon='lock'
+                      iconPosition='left'
+                      name='password'
+                      placeholder='Password'
+                      type='password'
                       onChange={this.handleChange}
                   />
                 </Segment>
-                <Header as="h2" textAlign="center">
-                  Payment Information
-                </Header>
-                <Segment stacked>
-                  <Form.Group inline>
+                <Divider horizontal>
+                 <Header inverted as="h2" textAlign="center">
+                   Payment Information
+                 </Header>
+                </Divider>
+                <Segment className='signup-form' stacked>
+                  <Form.Group>
                     <label>Payment Option</label>
+                    <Form.Field>
+                      Selected value: <b>{this.state.value}</b>
+                    </Form.Field>
                     <Form.Radio
                         label='Debit'
-                        name="radioGroup"
+                        name='card'
                         value = 'debit'
                         checked={this.state.value === 'debit'}
                         onChange={this.handleChange}
                     />
                     <Form.Radio
                         label='Credit'
-                        name="radioGroup"
+                        name='card'
                         value = "credit"
                         checked={this.state.value === 'credit'}
                         onChange={this.handleChange}
                     />
                   </Form.Group>
                   <Form.Input
-                      label="Card Number"
-                      icon="credit card"
-                      iconPosition="left"
-                      name="card number"
-                      placeholder="0000-0000-0000"
-                      type="text"
+                      label='Card Number'
+                      icon='credit card'
+                      iconPosition='left'
+                      name='cardNumber'
+                      placeholder='0000-0000-0000'
+                      type='text'
                       onChange={this.handleChange}
                   />
                   <Form.Group widths='equal'>
                     <Form.Input
                         fluid label='Expire Date'
+                        name='expireDate'
                         placeholder='00/00'
                         onChange={this.handleChange}
                     />
                     <Form.Input
                         fluid label='CVC'
+                        name='cvc'
                         placeholder='000'
                         onChange={this.handleChange}
                     />
                   </Form.Group>
                 </Segment>
-                <Form.Button content="Submit"/>
+                <div align='center'>
+                  <Form.Button secondary className='signup-button' content="SIGN UP"/>
+                </div>
               </Form>
-              <Message>
-                Already have an account? Login <Link to="/signin">here</Link>
+              <Message className='signup-message'>
+                <div align='center'>
+                 <Link to="/signin">Already have an account? Login here</Link>
+                </div>
               </Message>
               {this.state.error === '' ? (
                   ''
