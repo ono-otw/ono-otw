89a0b598f69b8fdab2f0c53a0e8ec92debc7372a
Sum-Tim <timsumalinogiii@gmail.com>
2020-04-25 17:31:11 -1000

    Finished Detail Component, Gonna Start on Final Bottom Section

--- app/imports/ui/components/Landing/Detail.jsx
+++ app/imports/ui/components/Landing/Detail.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import { Grid, Image, Header } from 'semantic-ui-react';
 import '../../styles/Landing/Detail';
 
-const Detail = () => {
+const Detail = props => {
+    const {header, description, image} = props;
     return (
         <Grid className="detail" centered>
-            <Grid.Row columns="2">
-                <Grid.Column>
-                    <Image className="" src='images/Blob.png' />   
+            <Grid.Row >
+                <Grid.Column width="4">
+                    <Image className="fillerImage" src={image} />   
                 </Grid.Column>
-                <Grid.Column>
-                   <div className="description">
-                        <Header content="oof" />
-                        <Header>Ooga booga</Header>
+                <Grid.Column width="1"></Grid.Column>
+                <Grid.Column className="description" width="10">
+                   <div className="descriptionContent">
+                        <Header content={header} />
+                        <Header content={description} />
                    </div>
+                   <div className="descriptionBkg"></div>   
                 </Grid.Column>
             </Grid.Row>
         </Grid>
@@ -21,3 +24,4 @@ const Detail = () => {
 }
 
 export default Detail;
+
