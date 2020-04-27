3bfecb4fdc0c090804e06401489388cbac618bf0
ruizaj <ruizaj@hawaii.edu>
2020-04-26 18:39:17 -1000

    Testing cart implemented into mongodb

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,7 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
-
+import { Cart } from '../../api/cart/Cart.js';
 /* eslint-disable no-console */
+
+const testingCart = JSON.parse(Assets.getText('testingCart.json'));
 
 /** Initialize the database with a default data document. */
 function addData(data) {
@@ -14,5 +16,18 @@ if (Stuffs.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
+  }
+}
+
+/** Initialize the database with a default data document. */
+function addCart(data) {
+  console.log(`  Adding: ${data.name} (${data.owner})`);
+  Stuffs.insert(data);
+}
+
+if (Cart.find().count() === 0) {
+  if (testingCart) {
+    console.log('Creating testing cart data.');
+    testingCart.map(data => addCart(data));
   }
 }
