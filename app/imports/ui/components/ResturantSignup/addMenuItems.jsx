4c95cb2b3d40baa9233eb7d848ed072dde578d17
Sum-Tim <timsumalinogiii@gmail.com>
2020-05-01 13:08:53 -1000

    Set up

---
+++ app/imports/ui/components/ResturantSignup/addMenuItems.jsx
@@ -0,0 +1,104 @@
+
+
+import React, { useState, Fragment } from 'react';
+import { Grid, Divider, Header, Container, Segment, Form, Button, FormField } from 'semantic-ui-react';
+import SimpleSchema from 'simpl-schema';
+import { number } from 'prop-types';
+
+// New Schema
+const menuItemsSchema = new SimpleSchema({
+    name: String,
+    prices: {
+        small: number,
+        medium: number,
+        large: number
+    },
+});
+
+const addMenuItems = () => {
+    
+    const submit = (data, formRefs) => {
+
+    }
+    
+    const [menu, setMenu] = useState([""]);
+
+    const addMenuItem = () => {
+        setMenu([...menu, ""])
+    }
+
+    const [prices, setPrices] = useState({
+        small: 0,
+        medium: 0,
+        large: 0
+    })
+
+    const handlePriceChange = (event) => {
+        const { name, value } = event.target;
+        setPrices((prevState) => ({
+            ...prevState,
+            [name]: value
+        }));
+    }
+
+    console.log(prices)
+
+    return (
+        <Fragment>
+            <Grid.Column width="16">
+                <Divider horizontal>
+                    <Header inverted as="h2" textAlign="center" content="Menu" />
+                </Divider>
+            </Grid.Column>
+            <Grid.Column centered width="8">
+                {menu.map(() => {
+                        return (<Segment  style={{backgroundColor: "#D3E3FC", borderRadius: "20px", }}>
+                            <Form>
+                                <FormField>
+                                    <label>Item</label>
+                                    <input style={{width: "60%"}} type="text" placeholder="Ex. Chicken Katsu"/>
+                                </FormField>
+                                <FormField>
+                                    <label>Prices</label>
+                                    <ul style={{listStyleType: "none"}}>
+                                        <li style={{width: "40%"}}>
+                                            <input onChange={handlePriceChange} 
+                                                type="number" 
+                                                placeholder="Small Price"
+                                                name="small"
+                                            />
+                                        </li>
+                                        <li style={{width: "40%"}}>
+                                            <input  onChange={handlePriceChange} 
+                                                type="number" 
+                                                placeholder="Medium Price"
+                                                name="medium"
+                                            />
+                                        </li>
+                                        <li style={{width: "40%"}}>
+                                            <input onChange={handlePriceChange} 
+                                                type="number" 
+                                                placeholder="Large Price" 
+                                                name="large"
+                                            />
+                                        </li>
+                                    </ul>
+                                </FormField>
+                                <Form.Field style={{width: "19%"}}>
+                                    <label>Resturant Image</label>
+                                    <input style={{ backgroundColor: "#D3E3FC", border: "none"}} type="file" />
+                                </Form.Field>
+                            </Form>
+                        </Segment>)
+            })}
+            <Button 
+                style={{backgroundColor: "#184470", color: "white"}}
+                content="Add another menu item"
+                onClick={addMenuItem}
+            />
+            </Grid.Column>
+    </Fragment>
+    ) 
+}
+
+export default addMenuItems;
--- app/imports/ui/pages/ResturantSignup.jsx
+++ app/imports/ui/pages/ResturantSignup.jsx
@@ -1,16 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Grid, Divider, Header, Container, Segment, Form, Button, FormField } from 'semantic-ui-react';
-
+import AddMenuItems from '../components/ResturantSignup/addMenuItems';
 
 
 
 const ResturantSignup = () => {
     // const [itemNumber, setItemNumber] = useState(1)
-    const [menu, setMenu] = useState([""]);
-
-    const addMenuItem = () => {
-        setMenu([...menu, ""])
-    }
+    
     return (
        <Container>
             <Grid centered>
@@ -39,47 +35,7 @@ const ResturantSignup = () => {
                         </Form>
                     </Segment>
                 </Grid.Column>
-
-                <Grid.Column width="16">
-                    <Divider horizontal>
-                        <Header inverted as="h2" textAlign="center" content="Menu" />
-                    </Divider>
-                </Grid.Column>
-                <Grid.Column centered width="8">
-                   {menu.map(number => {
-                        return (<Segment  style={{backgroundColor: "#D3E3FC", borderRadius: "20px", }}>
-                            <Form>
-                                <FormField>
-                                    <label>Item</label>
-                                    <input style={{width: "60%"}} type="text" placeholder="Ex. Chicken Katsu"/>
-                                </FormField>
-                                <FormField>
-                                    <label>Prices</label>
-                                    <ul style={{listStyleType: "none"}}>
-                                        <li style={{width: "40%"}}>
-                                            <input placeholder="Small Price"/>
-                                        </li>
-                                        <li style={{width: "40%"}}>
-                                            <input placeholder="Medium Price"/>
-                                        </li>
-                                        <li style={{width: "40%"}}>
-                                            <input placeholder="Large Price" />
-                                        </li>
-                                    </ul>
-                                </FormField>
-                                <Form.Field style={{width: "19%"}}>
-                                    <label>Resturant Image</label>
-                                    <input style={{ backgroundColor: "#D3E3FC", border: "none"}} type="file" />
-                                </Form.Field>
-                            </Form>
-                        </Segment>)
-                   })}
-                    <Button 
-                        style={{backgroundColor: "#184470", color: "white"}}
-                        content="Add another menu item"
-                        onClick={addMenuItem}
-                     />
-                </Grid.Column>
+                <AddMenuItems />
         </Grid>
        </Container>
     );
