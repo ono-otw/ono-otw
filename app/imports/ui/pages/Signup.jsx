dc3698ee6053b1b3dcbd4d76bc5c8f9d232db4b9
Jenny <jhsu2@hawaii.edu>
2020-05-02 21:42:11 -1000

    Added firstName/lastName for default accounts

--- app/imports/ui/pages/Signup.jsx
+++ app/imports/ui/pages/Signup.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link, Redirect } from 'react-router-dom';
-import { Container, Form, Grid, Header, Message, Segment, Divider, Button } from 'semantic-ui-react';
+import { Container, Form, Grid, Header, Message, Segment, Divider } from 'semantic-ui-react';
 import { Accounts } from 'meteor/accounts-base';
 import { Profile } from '../../api/profile/Profile';
 
