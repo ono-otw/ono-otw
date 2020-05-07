e1a4620b98a0ba8cbf650fab835dee0f91f4d497
Jenny <jhsu2@hawaii.edu>
2020-05-06 19:38:04 -1000

    Search works, need to work on redirecting

--- app/imports/ui/pages/Restaurants.jsx
+++ app/imports/ui/pages/Restaurants.jsx
@@ -4,44 +4,35 @@ import { Meteor } from 'meteor/meteor';
 import { Container, Card, Loader, Search, Icon, Header, Message } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import { Restaurant } from '../../api/restaurant/Restaurant';
 import RestaurantCard from '../components/RestaurantCard';
 
-/* Used to render search results. Atm the required fields for search using native semantic UI:
-* Title, Image, Description, Price
-* Will have figure out how to modify to read from database or change native search names from
-* semantic ui.
-*/
+const initialState = { isLoading: false, results: [], value: '', redirect: false };
 
-
-// const restaurantSearch = [{
-//   title: 'Raising Canes', description: '2615 S King St Unit 102',
-//   image: 'https://tinyurl.com/y7adk236',
-//   time: '15min', price: '4.5', label: ['Chicken'],
-// },
-//   {
-//     title: 'Bale', description: '2465 Campus Rd #220',
-//     image: 'https://s3-media0.fl.yelpcdn.com/bphoto/n3jOrjcJOVoz0npBf_FS1Q/o.jpg',
-//     time: '10min', price: '4.1', label: ['Sandwich', 'Pho'],
-//   },
-//   {
-//     title: 'Starbucks', description: '2465 Campus Rd #220.',
-//     image: 'https://assets.change.org/photos/7/ou/zi/OlOuziNRVcXqzpX-800x450-noPad.jpg?1531499872',
-//     time: '8min', price: '4.5', label: ['Coffee', 'Tea'],
-//   },
-// ];
-
-const initialState = { isLoading: false, results: [], value: '' };
 /** Renders the list of restaurants */
 class Restaurants extends React.Component {
 
-  searchOptions = () => {
-    return _.map(this.props.restaurant, function (document) { return { title: document.name, description: document.address, image: document.bgimg, time: document.time, price: document.rating, label: document.label }; });
-  };
+  // eslint-disable-next-line max-len
+  searchOptions = () => _.map(this.props.restaurant, function (document) { return { title: document.name, description: document.address, image: document.bgimg, time: document.time, price: document.rating, label: document.label }; });
 
   state = initialState;
 
-  handleResultSelect = (e, { result }) => this.setState({ value: result.title });
+  handleResultSelect = (e, { result }) => {
+    this.setState({
+      value: result.title,
+      redirect: true });
+  };
+
+  renderRedirect = (menu) => {
+    if (this.state.redirect) {
+      return <Redirect to={{
+        pathname: '/menu',
+        state: { id: `${menu}` },
+      }}
+      />;
+    }
+  };
 
   handleSearchChange = (e, { value }) => {
     this.setState({ isLoading: true, value });
@@ -51,14 +42,13 @@ class Restaurants extends React.Component {
       if (this.state.value.length < 1) return this.setState(initialState);
 
       const re = new RegExp(_.escapeRegExp(this.state.value), 'i');
-      const isMatch = (result) => re.test(result.name);
+      const isMatch = (result) => re.test(result.title);
 
       this.setState({
         isLoading: false,
-        results: _.filter(this.searchOptions, isMatch),
+        results: _.filter(this.searchOptions(), isMatch),
       });
     }, 300);
-    console.log(this.searchOptions());
   };
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
@@ -80,8 +70,6 @@ class Restaurants extends React.Component {
     };
 
     const { isLoading, value, results } = this.state;
-    console.log(results);
-    console.log(value);
 
     return (
         <Container>
@@ -97,13 +85,14 @@ class Restaurants extends React.Component {
             <Message className='restaurant-message'>
               <Message.Header>To Search....</Message.Header>
               <Message.List>
-                <Message.Item>Search by food category</Message.Item>
+                <Message.Item>Search by Restaurant&apos;s name</Message.Item>
                 {/* eslint-disable-next-line react/no-unescaped-entities */}
                 <Message.Item>Clicking on Restaurant's title will bring you to the menu page. </Message.Item>
               </Message.List>
             </Message>
           </div>
           <div align='center' style={searchBar}>
+            {this.renderRedirect(this.props.restaurant._id)}
             <Search className='search_bar'
                     input={{ icon: 'search', iconPosition: 'left' }}
                     loading={isLoading}
@@ -117,7 +106,8 @@ class Restaurants extends React.Component {
             />
           </div>
             <Card.Group itemsPerRow='4' centered className='restaurant_card'>
-                {this.props.restaurant.map((restaurant, index) => <RestaurantCard key={index} restaurant={restaurant}/>)}
+              {this.props.restaurant.map((restaurant, index) => <
+                  RestaurantCard key={index} restaurant={restaurant}/>)}
             </Card.Group>
         </Container>
     );
