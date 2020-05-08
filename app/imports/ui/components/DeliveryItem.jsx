75b075daeaf617e8ac1f002a8fec205aa023a0ac
wjinyan <47268740+wjinyan@users.noreply.github.com>
2020-05-07 20:04:04 -1000

    Update imports to PastDelivery

--- app/imports/ui/components/DeliveryItem.jsx
+++ app/imports/ui/components/DeliveryItem.jsx
@@ -10,15 +10,16 @@ class DeliveryItem extends React.Component {
   finishOrder(docID) {
     const owner = Meteor.user().username;
     const store = this.props.order.store;
-    const firstName = this.props.order.firstName;
-    const lastName = this.props.order.lastName;
-    const image = this.props.order.image;
-    const venmo = this.props.order.venmo;
-    const quantity = this.props.order.quantity;
-    const personWhoOrdered = this.props.order.personWhoOrdered;
-    const location = this.props.order.location;
-    const name = this.props.order.name;
+    const orderTime = new Date();
+    const monthOption = { month: 'long' };
+    const month = new Intl.DateTimeFormat('en-US', monthOption).format(orderTime);
+    const day = orderTime.getDate();
+    const weekdayOption = { weekday: 'long' };
+    const weekday = new Intl.DateTimeFormat('en-US', weekdayOption).format(orderTime);
+    const item = _.reduce(this.props.order.quantity, (total, current) => (current + total), 0);
+    const cost = this.props.order.cost;
 
+    console.log(cost);
     swal({
       title: 'Wait!',
       text: 'Are you sure you delivered the order?',
@@ -29,8 +30,7 @@ class DeliveryItem extends React.Component {
           if (accept) {
             console.log('Accepting order');
             PastDelivery.insert({
-              name, firstName, lastName, image, store, owner, venmo, quantity,
-              personWhoOrdered, location,
+              owner, store, month, day, weekday, item, cost,
             });
             AcceptedOrders.remove(docID);
             this.forceUpdate();
