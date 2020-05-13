d1f969eed29f736205a0fc3e5f04497a0be1a6f1
Jenny <jhsu2@hawaii.edu>
2020-05-12 18:09:15 -1000

    AddRestaurant linked to Admin page

---
+++ app/imports/ui/components/RestaurantApprove.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Grid, Header, Button, List, Modal, Card } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import swal from 'sweetalert';
+import { Restaurant } from '../../api/restaurant/Restaurant';
+import RestaurantCard from './RestaurantCard';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+class RestaurantApprove extends React.Component {
+  updateItem(docID) {
+    console.log(`restaurant to delete is: ${docID}`);
+    swal({
+      title: 'Do you want to approve this restaurant?',
+      dangerMode: true,
+      icon: 'warning',
+      buttons: true,
+    })
+        /* eslint-disable-next-line */
+        .then((update) => {
+          if (update) {
+            Restaurant.update(
+                { _id: docID },
+                {
+                  $set: {
+                    approved: true,
+                  },
+                },
+            );
+            swal('This restaurant has been approved!', {
+              icon: 'success',
+            });
+          } else {
+            swal('Restaurant not approved.');
+          }
+        });
+  }
+
+  render() {
+
+    return (
+        <div>
+          <List>
+            <Grid columns={5}>
+              <Grid.Column>
+                <Header>{this.props.restaurants.name}</Header>
+              </Grid.Column>
+              <Grid.Column>
+                <Modal trigger={
+                  <Button basic color={'grey'}>view</Button>
+                } closeIcon
+                       style={{
+                         background: 'transparent', border: '0',
+                         boxShadow: '0px 0px 0px 0px rgba(34, 36, 38, 0.12), ' +
+                             '0px 0px 0px 0px rgba(34, 36, 38, 0.15)',
+                         width: '18%',
+                       }}
+                       size={'small'}>
+                  <Card.Group centered className='restaurant_card'>
+                    <RestaurantCard restaurant={this.props.restaurants}/>
+                  </Card.Group>
+                </Modal>
+              </Grid.Column>
+              <Grid.Column>
+                <Button className='accept'
+                        onClick={() => this.updateItem(this.props.restaurants._id)}>Accept</Button>
+                <Button className='deny'>Deny</Button>
+              </Grid.Column>
+            </Grid>
+          </List>
+
+        </div>
+    );
+  }
+}
+
+/** Require a document to be passed to this component. */
+RestaurantApprove.propTypes = {
+  restaurants: PropTypes.object.isRequired,
+};
+
+/** Wrap this component in withRouter since we use the <Link> React Router element. */
+export default withRouter(RestaurantApprove);
--- app/imports/ui/pages/Admin.jsx
+++ app/imports/ui/pages/Admin.jsx
@@ -3,33 +3,12 @@ import { Container, List, Header, Loader, Button, Grid } from 'semantic-ui-react
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
-import swal from 'sweetalert';
-
 import { Restaurant } from '../../api/restaurant/Restaurant';
 import RestaurantTable from '../components/RestaurantTable';
+import RestaurantApprove from '../components/RestaurantApprove';
 
 class Admin extends React.Component {
-  removeItem(docID) {
-    console.log(`restaurant to delete is: ${docID}`);
-    swal({
-      title: 'Do you really want to delete this restaurant?',
-      text: 'You cant get it back once you do fam',
-      dangerMode: true,
-      icon: 'warning',
-    })
 
-    .then((willDelete) => {
-      if (willDelete) {
-        Restaurant.remove(docID);
-
-        swal('Poof! This restaurant item has been deleted!', {
-          icon: 'success',
-        });
-      } else {
-        swal('You canceled the deletion!');
-      }
-    });
-  }
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
@@ -47,26 +26,8 @@ class Admin extends React.Component {
                 key={index} restaurants={restaurants}/>)}
             <Header as='h1' style={{ marginTop: '50px' }}>Requests</Header>
             <hr className='blackBorder'/>
-            <List>
-              <Grid columns={5}>
-                <Grid.Column><Header as='h3'>Raising Canes</Header></Grid.Column>
-                <Grid.Column>View</Grid.Column>
-                <Grid.Column>
-                  <Button className='accept'>Accept</Button>
-                  <Button className='deny'>Deny</Button>
-                </Grid.Column>
-              </Grid>
-            </List>
-            <List>
-            <Grid columns={5}>
-              <Grid.Column><Header as='h3'>Shaka Shaka</Header></Grid.Column>
-              <Grid.Column>View</Grid.Column>
-              <Grid.Column>
-                <Button className='accept'>Accept</Button>
-                <Button className='deny'>Deny</Button>
-              </Grid.Column>
-            </Grid>
-          </List>
+            {this.props.restaurantsApproval.map((restaurants, index) => <RestaurantApprove
+                key={index} restaurants={restaurants}/>)}
           </div>
         </Container>
     );
@@ -76,6 +37,7 @@ class Admin extends React.Component {
 /** Require an array of Stuff documents in the props. */
 Admin.propTypes = {
   restaurants: PropTypes.array.isRequired,
+  restaurantsApproval: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
@@ -84,7 +46,8 @@ export default withTracker(() => {
   // Get access to Menu documents.
   const subscription = Meteor.subscribe('Restaurant');
   return {
-    restaurants: Restaurant.find({}).fetch(),
+    restaurants: Restaurant.find({ approved: true }).fetch(),
+    restaurantsApproval: Restaurant.find({ approved: false }).fetch(),
     ready: subscription.ready(),
   };
 })(Admin);
