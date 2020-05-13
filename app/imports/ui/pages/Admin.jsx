9f06faad9331e9fac40c8bd8e1ef9fa972af4879
wjinyan <47268740+wjinyan@users.noreply.github.com>
2020-05-12 16:24:39 -1000

    Update Admin.jsx

--- app/imports/ui/pages/Admin.jsx
+++ app/imports/ui/pages/Admin.jsx
@@ -3,33 +3,11 @@ import { Container, List, Header, Loader, Button, Grid } from 'semantic-ui-react
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
-import swal from 'sweetalert';
 
 import { Restaurant } from '../../api/restaurant/Restaurant';
 import RestaurantTable from '../components/RestaurantTable';
 
 class Admin extends React.Component {
-  removeItem(docID) {
-    console.log(`restaurant to delete is: ${docID}`);
-    swal({
-      title: 'Do you really want to delete this restaurant?',
-      text: 'You cant get it back once you do fam',
-      dangerMode: true,
-      icon: 'warning',
-    })
-
-    .then((willDelete) => {
-      if (willDelete) {
-        Restaurant.remove(docID);
-
-        swal('Poof! This restaurant item has been deleted!', {
-          icon: 'success',
-        });
-      } else {
-        swal('You canceled the deletion!');
-      }
-    });
-  }
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
